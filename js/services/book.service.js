2828aeaa4a6d1179fa77798812450675675cf01d
omerKaduri1 <kaduriomer@gmail.com>
2024-02-15 16:10:55 +0200

    Add books url variables

--- js/services/book.service.js
+++ js/services/book.service.js
@@ -1,23 +1,26 @@
 'use strict'
 
+var harryPotterUrl = 'https://wizkids.co.il/cdn/shop/products/02e5ee995b80d61e09a9849df5c83513_x700.gif?v=1514210286'
+var worldUrl = 'https://shop.lonelyplanet.com/cdn/shop/products/the-world-lonely-planet-s-guide-to_900x.jpg?v=1667661690'
+var hungerGamesUrl = 'https://wizkids.co.il/cdn/shop/products/9780439023528_x700.jpg?v=1570375084'
 var gBooks = [
     {
         id: makeId(),
         title: 'Harry Potter',
         price: 200,
-        imgUrl: 'img/Harry-potter.webp'
+        imgUrl: harryPotterUrl
     },
     {
         id: makeId(),
         title: 'The World',
         price: 150,
-        imgUrl: 'img/the-world.webp'
+        imgUrl: worldUrl
     },
     {
         id: makeId(),
         title: 'The Hunger Games',
         price: 100,
-        imgUrl: 'img/the-hunger-games.webp'
+        imgUrl: hungerGamesUrl
     }
 ]
 
