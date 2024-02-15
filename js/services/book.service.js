347354aef29639ed313966852ab92f242aed6769
omerKaduri1 <kaduriomer@gmail.com>
2024-02-15 19:19:01 +0200

    Create and add local storage

--- js/services/book.service.js
+++ js/services/book.service.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const BOOK_DB = 'bookDB'
 var harryPotterUrl = 'https://wizkids.co.il/cdn/shop/products/02e5ee995b80d61e09a9849df5c83513_x700.gif?v=1514210286'
 var worldUrl = 'https://shop.lonelyplanet.com/cdn/shop/products/the-world-lonely-planet-s-guide-to_900x.jpg?v=1667661690'
 var hungerGamesUrl = 'https://m.media-amazon.com/images/I/71WSzS6zvCL._AC_UF1000,1000_QL80_.jpg'
 var defaultUrl = 'https://wizkids.co.il/cdn/shop/products/Itstartswithus_x700.jpg?v=1669823430'
 
+const BOOK_DB = 'bookDB'
 var gBooks
 _createBooks()
 
@@ -14,26 +14,26 @@ function getBooks() {
 }
 
 function removeBook(bookId) {
-    console.log(bookId);
     const bookIdx = gBooks.findIndex(book => book.id === bookId)
-    console.log(bookIdx);
-    gBooks.splice(bookIdx, 1)
+    if (bookIdx !== -1) gBooks.splice(bookIdx, 1)
+    _saveBooks()
 }
 
 function updatePrice(bookId, newPrice) {
     const book = gBooks.find(book => book.id === bookId)
     book.price = (newPrice) ? newPrice : book.price
+    _saveBooks()
 }
 
 function addBook(title, price) {
     if (!title || !price) return
     const newBook = _createBook(title, price)
     gBooks.push(newBook)
-
+    _saveBooks()
 }
 
 function _createBooks() {
-    // gBooks = loadFromStorage()
+    gBooks = loadFromStorage('bookDB')
 
     if (!gBooks) {
         gBooks = [
@@ -41,6 +41,7 @@ function _createBooks() {
             _createBook('The World', 150, worldUrl),
             _createBook('The Hunger Games', 100, hungerGamesUrl),
         ]
+        _saveBooks()
     }
 }
 
@@ -55,4 +56,8 @@ function _createBook(title, price, imgUrl = defaultUrl) {
 
 function getBookById(bookId) {
     return gBooks.find(book => book.id === bookId)
+}
+
+function _saveBooks() {
+    saveToStorage(BOOK_DB, gBooks)
 }
